e9d2ce7c81fd29416b7d211885313e7e718576e3
Your Name <you@example.com>
2025-09-04 23:00:51 +0800

    deploy kamis

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,119 +20,121 @@ function handleMenuClick(id) {
 </script>
 
 <template>
-  <header
-    class="sticky top-0 z-20 bg-white px-4 md:px-10 lg:px-14 py-6 flex items-center justify-between"
-  >
-    <h1 class="text-3xl font-bold text-shadow-sm text-blue-800">Portfolio</h1>
-    <!-- Laptop dan Dekstop -->
-    <nav class="hidden md:flex">
-      <ul class="flex space-x-8 font-medium text-md">
-        <li>
-          <a
-            href=""
-            @click.prevent="scrollToSection('section1')"
-            class="text-blue-800 hover:text-blue-800/90"
-          >
-            Home
-          </a>
-        </li>
-
-        <li>
-          <a
-            @click.prevent="scrollToSection('section3')"
-            href=""
-            class="text-blue-800 hover:text-blue-800/90"
-            >Skills & Tools</a
-          >
-        </li>
-        <li>
-          <a
-            @click.prevent="scrollToSection('section4')"
-            href=""
-            class="text-blue-800 hover:text-blue-800/90"
-            >My Projects</a
-          >
-        </li>
-        <li>
-          <a
-            @click.prevent="scrollToSection('section5')"
-            href=""
-            class="text-blue-800 hover:text-blue-800/90"
-            >Contact</a
-          >
-        </li>
-      </ul>
-    </nav>
-
-    <!-- Hamburger Button -->
-    <button
-      @click="openSidebar = true"
-      class="md:hidden text-blue-800 focus:outline-none cursor-pointer"
+  <header class="sticky top-1 z-20 px-2">
+    <div
+      class="bg-white px-5 md:px-8 lg:px-12 py-5 flex items-center justify-between rounded-full shadow-lg shadow-blue-800/20"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-7 w-7"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
+      <h1 class="text-3xl font-bold text-shadow-sm text-blue-800">Portfolio</h1>
+      <!-- Laptop dan Dekstop -->
+      <nav class="hidden md:flex">
+        <ul class="flex space-x-8 font-medium text-md">
+          <li>
+            <a
+              href=""
+              @click.prevent="scrollToSection('section1')"
+              class="text-blue-800 hover:text-blue-800/90"
+            >
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a
+              @click.prevent="scrollToSection('section3')"
+              href=""
+              class="text-blue-800 hover:text-blue-800/90"
+              >Skills & Tools</a
+            >
+          </li>
+          <li>
+            <a
+              @click.prevent="scrollToSection('section4')"
+              href=""
+              class="text-blue-800 hover:text-blue-800/90"
+              >My Projects</a
+            >
+          </li>
+          <li>
+            <a
+              @click.prevent="scrollToSection('section5')"
+              href=""
+              class="text-blue-800 hover:text-blue-800/90"
+              >Contact</a
+            >
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Hamburger Button -->
+      <button
+        @click="openSidebar = true"
+        class="md:hidden text-blue-800 focus:outline-none cursor-pointer"
       >
-        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-      </svg>
-    </button>
-
-    <!-- Sidebar -->
-    <div
-      class="fixed top-0 left-0 h-full w-64 bg-white text-blue-800 shadow-lg transform transition-transform duration-300 z-30"
-      :class="openSidebar ? 'translate-x-0' : '-translate-x-full'"
-    >
-      <div class="flex mt-5 px-5 py-5 justify-between items-center">
-        <h2 class="text-lg font-bold">Menu</h2>
-        <button
-          @click="openSidebar = false"
-          class="focus:outline-none text-blue-800 hover:text-blue-800/90"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-7 w-7"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-7 w-7"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <ul class="flex flex-col mt-6 space-y-5 px-5 text-md">
-        <li>
-          <a @click="handleMenuClick('section1')" class="text-blue-800 hover:text-blue-800/90"
-            >Home</a
-          >
-        </li>
-        <li>
-          <a @click="handleMenuClick('section3')" class="text-blue-800 hover:text-blue-800/90"
-            >Skills & Tools</a
-          >
-        </li>
-        <li>
-          <a @click="handleMenuClick('section4')" class="text-blue-800 hover:text-blue-800/90"
-            >My Projects</a
-          >
-        </li>
-        <li>
-          <a @click="handleMenuClick('section5')" class="text-blue-800 hover:text-blue-800/90"
-            >Contact</a
-          >
-        </li>
-      </ul>
-    </div>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
 
-    <!-- Overlay -->
-    <div
-      v-if="openSidebar"
-      @click="openSidebar = false"
-      class="fixed inset-0 backdrop-blur-xs transition-opacity z-20"
-    ></div>
+      <!-- Sidebar -->
+      <div
+        class="fixed top-5 left-0 h-full w-64 bg-white text-blue-800 shadow-lg transform transition-transform duration-300 z-50"
+        :class="openSidebar ? 'translate-x-0' : '-translate-x-full'"
+      >
+        <div class="flex px-5 py-5 justify-between items-center">
+          <h2 class="text-lg font-bold">Menu</h2>
+          <button
+            @click="openSidebar = false"
+            class="focus:outline-none text-blue-800 hover:text-blue-800/90 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+        <ul class="flex flex-col mt-6 space-y-5 px-5 text-md">
+          <li>
+            <a @click="handleMenuClick('section1')" class="text-blue-800 hover:text-blue-800/90"
+              >Home</a
+            >
+          </li>
+          <li>
+            <a @click="handleMenuClick('section3')" class="text-blue-800 hover:text-blue-800/90"
+              >Skills & Tools</a
+            >
+          </li>
+          <li>
+            <a @click="handleMenuClick('section4')" class="text-blue-800 hover:text-blue-800/90"
+              >My Projects</a
+            >
+          </li>
+          <li>
+            <a @click="handleMenuClick('section5')" class="text-blue-800 hover:text-blue-800/90"
+              >Contact</a
+            >
+          </li>
+        </ul>
+      </div>
+
+      <!-- Overlay -->
+      <div
+        v-if="openSidebar"
+        @click="openSidebar = false"
+        class="fixed inset-0 backdrop-blur-xs transition-opacity z-20"
+      ></div>
+    </div>
   </header>
 </template>
